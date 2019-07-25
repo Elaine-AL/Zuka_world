42a796b5d8df8d49a0ce08f0bf78f2bc9ca11e42
Catherine Gitau <catherinegitau94@gmail.com>
2019-07-25 15:06:40 +0300

    included instructor and student folders

---
+++ data/data_exploration.R
@@ -0,0 +1,7 @@
+#load in the necessary libraries
+library(tidyverse)
+
+#load in the data
+kiva_locations <- read_csv("/home/catherine/Desktop/Zuka/Zuka_world/data/Kenya/kiva_ke_locations.csv")
+kiva_loans <- read_csv("/home/catherine/Desktop/Zuka/Zuka_world/data/Kenya/kiva_loans_ke.csv")
+kiva_themes <- read_csv("/home/catherine/Desktop/Zuka/Zuka_world/data/Kenya/loan_theme_ke.csv")
